18920a7406e571d4e8664ca7e8a31a6be7d098ca
levmarc19-svg <levmarc19@ecolecatholique.ca>
2026-05-20 12:20:12 -0400

    Delete music2.js

--- music2.js
+++
@@ -1,6 +0,0 @@
- const audio = document.getElementById('music');
-    audio.play().catch(() => {
-      document.addEventListener('click', () => audio.play(), { 
-        once: true 
-                                                             });
-    });
