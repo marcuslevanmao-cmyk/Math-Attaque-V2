ee94e37701c97424ab679278ac5ca71855ddf16e
levmarc19-svg <levmarc19@ecolecatholique.ca>
2026-05-20 21:17:08 -0400

    Update intro.js

--- intro.js
+++ intro.js
@@ -14,27 +14,27 @@ function play(video, next, fallbackTime = 6000) {
             next()                
         }
     }).catch(() => {
-        // Si la lecture échoue (ex: autoplay bloqué), on passe quand même
+        // la lecture échoue on passe quand même
         clearTimeout(fallback)
         next()
     })
 }
 
-// Étape 1  affiche la vidéo d'ouverture
+//affiche la vidéo d'ouverture
 function startIntro() {
     opening.hidden = false  
     loading.hidden = true  
     play(opening, startLoading, 8000) 
 }
 
-// Étape 2  affiche l'écran de chargement
+// affiche l'écran de chargement
 function startLoading() {
     opening.hidden = true 
     loading.hidden = false
     play(loading, goToHome, 10000) 
 }
 
-// Étape 3  redirige vers la page d'accueil
+// redirige vers la page d'accueil
 // https://developer.mozilla.org/fr/docs/Web/API/Location/href
 function goToHome() {
     window.location.href = "homepage.html"
